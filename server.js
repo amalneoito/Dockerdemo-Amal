b32fbb6b4627b1b9775791a64eea360d99c09f22
Ubuntu <ubuntu@ip-172-31-39-3.ap-south-1.compute.internal>
2022-09-21 08:47:55 +0000

    updated the pipeline codes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello  World im amal and  i  did it!!!!');
+  res.send('Hey Docker  im amal!!!!');
 });
 
 app.listen(PORT, HOST);
